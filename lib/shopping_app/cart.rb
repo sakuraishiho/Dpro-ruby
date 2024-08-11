94226c088da3d6b6d4649a4ad30a20ac5a6ea7a8
sakura <ichigomilk35412@gmail.com>
2024-08-11 09:51:46 +0900

    initial commit

---
+++ lib/shopping_app/cart.rb
@@ -0,0 +1,43 @@
+require_relative "item_manager"
+require_relative "ownable"
+
+
+class Cart
+  include ItemManager
+  include Ownable
+
+
+  def initialize(owner)
+    self.owner = owner
+    @items = []
+  end
+
+  def items
+    # Cartにとってのitemsは自身の@itemsとしたいため、ItemManagerのitemsメソッドをオーバーライドします。
+    # CartインスタンスがItemインスタンスを持つときは、オーナー権限の移譲をさせることなく、自身の@itemsに格納(Cart#add)するだけだからです。
+    @items
+  end
+
+  def add(item)
+    @items << item
+  end
+
+  def total_amount
+    @items.sum(&:price)
+  end
+
+
+  def check_out
+    return if owner.wallet.balance < total_amount
+
+      @items.each do |item|
+        
+        item.owner.wallet.deposit(item.price)
+        self.owner.wallet.withdraw(item.price)
+        item.owner = self.owner
+      end
+
+      
+      @items.clear
+    end
+  end
